47fc8328c3731f70c58f79c9b420d30c71e58e83
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-11-09 15:01:36 +0100

    add buttons with Angular Material styling in app component

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,10 +1,22 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {Component, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {MatButton} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-root',
-  template: './app.html',
+  template: `
+    <section>
+      <button matButton="elevated">elevated</button>
+      <button matButton="outlined">outlined</button>
+      <button matButton="filled">filled</button>
+      <button matButton="tonal" >tonal</button>
+      <button matButton>Basic</button>
+    </section>
+  `,
+  imports: [
+    MatButton
+  ]
 })
 export class App {}
 
